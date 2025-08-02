1f03d224351e94cc567bdcaaef85b83aef08801b
Alan Stewart <stewaala@gmail.com>
2025-08-02 11:12:27 +0000

    added identity copula function

---
+++ experiments/alan_copula_rct_identity.R
@@ -0,0 +1,104 @@
+library(causl)
+library(data.table)
+
+alan_copula_rct_identity <- function(n,
+                           rho        = 0.50,
+                           seed       = 111,
+                           p_base     = 0.20) {
+  
+  ##########################################################################################
+  # ensure the user has passed rho/p_base combination that will generate valid probabilities
+  ##########################################################################################
+  # 1) Restrict rho to allowable values
+  valid_rho <- c(0.00, 0.25, 0.50, 0.75)
+  if (! (rho %in% valid_rho) ) {
+    stop(sprintf(
+      "Invalid rho = %.2f: must be one of %s",
+      rho, paste(valid_rho, collapse = ", ")
+    ))
+  }
+  
+  # 2) Compute admissible p_base interval:
+  lower <- 0.17 + 0.067 * rho
+  upper <- 0.83 - 0.067 * rho
+  
+  # 3) Validate p_base
+  if (p_base < lower || p_base > upper) {
+    stop(sprintf(
+      "For rho = %.2f, p_base must be in [%.3f, %.3f]. You supplied %.3f.",
+      rho, lower, upper, p_base
+    ))
+  }
+  ##########################################################################################
+  
+  fam <- list(
+    c(1,3,4),
+    c(5,5,5,1,2),
+    c(1),
+    c(1,3,4)
+  )
+  
+  forms <- list(
+    c(X1~1, X2~X1, X3~X1),
+    list(A~1, C1~1, C2~C1, C3~C1:C2, C4~C1),
+    E~1,
+    ~ C2
+  )
+  
+  pars <- list(
+    X1=list(beta=0,phi=1),
+    X2=list(beta=c(0.1,0.2),phi=1),
+    X3=list(beta=c(0.1,0.1),phi=1),
+    A =list(beta=0),
+    C1=list(beta=0),
+    C2=list(beta=c(-2,1)),
+    C3=list(beta=c(0,0.1),phi=1),
+    C4=list(beta=c(0,0.1),phi=0.1,par2=20),
+    E =list(beta=0,phi=1),
+    cop=list(
+      E=list(
+        X1=list(beta=c( 0, 0.5)),
+        X2=list(beta=c(-0.5,0)),
+        X3=list(beta=c( 0.5,0))
+      )
+    )
+  )
+  
+  link <- list(
+    c("identity","log","logit"),
+    c("logit","logit","logit","identity","identity"),
+    "identity"
+  )
+  
+  set.seed(seed)
+  dat <- as.data.table(
+    rfrugalParam(n, formulas=forms, pars=pars,
+                 family=fam, link=link)
+  )
+
+  ## prognostic part (centred)
+  h  <- with(dat, 0.10*C1 + 0.12*sin(C4))
+  h_cent <- h - mean(h)
+  p0 <- p_base + h_cent
+  
+  fC      <- with(dat, (-C1 - C2 + (C3>0) + C4^2) / 3)
+  fC_cent <- fC - mean(fC)
+  delta   <- 0.10 * rho * fC_cent          # zero‑mean additive effect
+  
+  p1     <- p0 + delta                               # treated risk
+  
+  if (min(p0) < 0 || max(p1) > 1 || min(p1) < 0 || max(p0)>1)
+    stop("Choose smaller rho or baseline: probabilities out of bounds.")
+
+  CATE <- p1 - p0
+  dat[, Y := as.integer(pnorm(E) < ifelse(A==1, p1, p0))]
+  
+  dat[, `:=`(
+    CATE = CATE,
+    p0=p0,
+    p1=p1
+  )]
+  dat[, E := NULL]
+  
+  return(dat[])
+}
