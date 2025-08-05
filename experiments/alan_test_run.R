ca3a28846702d38948337f89390b9d5d5b15015d
Alan Stewart <stewaala@gmail.com>
2025-08-05 14:41:45 +0000

    cleaned up file system

--- experiments/alan_test_run.R
+++
@@ -1,8 +0,0 @@
-source('copula_rct.R')
-source('alan_run_trials.R')
-
-alan_run_trials(n=5000, rho=0.5, trials=100)
-
-
-# pvals <- read.csv("data/pvals_n00200_rho50.csv", stringsAsFactors = FALSE)
-# View(pvals)
